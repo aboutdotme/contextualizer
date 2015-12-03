faf1169bb9d46c067bcebf1f25fb6f09af512827
Nigel Kibodeaux <nigel@team.about.me>
2015-12-03 12:05:53 -0900

    such a dumb mistake

--- test.js
+++ test.js
@@ -141,9 +141,9 @@ describe('index.js', function() {
             async4(function(err) {
                 // console.log(err.stack)
                 var expected_lines = 6
-                if (process.version > 'v4') expected_lines++
+                if (process.versions.node > '4') ++expected_lines
                 var lines = err.stack.split('\n').length
-                assert.equal(lines, 7)
+                assert.equal(lines, expected_lines)
                 done()
             })
         })
